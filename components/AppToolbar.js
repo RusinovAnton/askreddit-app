d62d66bfbdad98dd1cfa2f8efff751c913416f49
Rusinov Anton <rusinovantondev@gmail.com>
2018-02-25 19:00:29 +0200

    Fix ToolbarAction button color

--- components/AppToolbar.js
+++ components/AppToolbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { Toolbar, ToolbarAction, ToolbarContent } from 'react-native-paper';
+import {
+  Toolbar,
+  ToolbarAction,
+  ToolbarContent,
+  TouchableRipple,
+} from 'react-native-paper';
 
 class AppToolbar extends React.Component {
   render() {
     return (
       <Toolbar statusBarHeight={Platform.OS === 'android' ? 20 : 0}>
         <ToolbarContent title="/r/Askreddit" dark />
-        <ToolbarAction icon="more-vert" color="#fff" onPress={this._onMore} />
+        <ToolbarAction icon="more-vert" dark />
       </Toolbar>
     );
   }
